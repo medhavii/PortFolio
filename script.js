b9e5707a5ca9f948e5f55350c0f9ae1584968046
medhavii <medhavimehta118@gmail.com>
2022-11-12 19:16:28 +0530

    done

--- script.js
+++ script.js
@@ -14,3 +14,30 @@ function opentab(tabName){
     document.getElementById(tabName).classList.add("active-tab");
 }
 
+const sideMenu = document.getElementById("sideMenu");
+
+function closeMenu(){
+    sideMenu.style.right=' -200px';
+    console.log("mcmc");
+}
+
+function openMenu(){
+    sideMenu.style.right='0';
+}
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbyB6eymHxbpTICvT1q-Z4Y55dIso4aM4z_woBtYhJ-lXiamP7l9TRRvWjAJNLZs3oyBpQ/exec'
+  const form = document.forms['submit-to-google-sheet']
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => {
+        msg.innerHTML="Message Sent Successfully!";
+        setTimeout(function(){
+            msg.innerHTML=""
+        },100000)
+        form.reset()
+      })
+      .catch(error => console.error('Error!', error.message))
+  })
+const msg = document.getElementById("msg");
